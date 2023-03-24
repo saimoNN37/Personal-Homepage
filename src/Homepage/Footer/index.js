01a387e4a7e13f45a3b0d6ec9843bb674424e89f
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-03-24 12:36:17 +0100

    small changes

--- src/Homepage/Footer/index.js
+++ src/Homepage/Footer/index.js
@@ -9,7 +9,7 @@ const Footer = () => (
         <AboutMe>
             I'm always open to new projects whenever I have the time. If you have a website,
             dashboard or mobile app in mind and need some help to make your ideas come to life,
-            feel free to conatct me
+            feel free to conatct me .
         </AboutMe>
         <IconsContainer>
             {socialMedia.map(({Icon, link, name}) =>(
--- src/Homepage/Footer/styled.js
+++ src/Homepage/Footer/styled.js
@@ -6,42 +6,42 @@ export const StyledFooter = styled.div`
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-start;
-    
 `;
 
 export const LetsTalk = styled.caption`
 font-weight: 700;
 font-size: 12px;
-color: ${({theme}) => theme.elementColor.footer.letsTalk};
+color: ${({ theme }) => theme.elementColor.footer.letsTalk};
 `;
 
 export const AddresEmail = styled.p`
     font-weight: 900;
     font-size: 32px;
     margin: 24px 0 24px;
-    color: ${({theme}) => theme.elementColor.footer.email};
+    color: ${({ theme }) => theme.elementColor.footer.email};
 `;
 
 export const AboutMe = styled.p`
 font-weight: 400;
 font-size: 18px;
 line-height: 1.4;
-color: ${({theme}) => theme.elementColor.footer.aboutMe};
+color: ${({ theme }) => theme.elementColor.footer.aboutMe};
 max-width: 670px;
 `;
 
 export const styledIcon = (Icon) => styled(Icon)`
 width: 48px;
+transition: 0.3s;
+color: ${({ theme }) => theme.elementColor.footer.icons};
+
+&:hover {
+    color: ${({ theme }) => theme.elementColor.footer.iconsHover};
+}
+
 `;
 
 export const Link = styled.a`
 text-decoration: none;
-color: ${({theme}) => theme.elementColor.footer.icons};
-transition: 0.3s;
-
-&:hover {
-    color: ${({theme}) => theme.elementColor.footer.iconsHover};
-}
 `;
 
 export const IconsContainer = styled.div`
