dd238377471c403b604dced5d4ea1918f4857661
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-04-17 12:06:53 +0200

    added store to app

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './core/App';
 import reportWebVitals from './reportWebVitals';
+import store from './core/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
