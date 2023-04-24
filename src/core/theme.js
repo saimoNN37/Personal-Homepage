96a436653490a768f28f609f70d9ff22303c4e83
Szymon Mlynski <szymon.mlynski03@gmail.com>
2023-04-24 14:56:11 +0200

    added darkmode colors

--- src/core/theme.js
+++ src/core/theme.js
@@ -8,6 +8,10 @@ anakiwa: "8CC2FF",
 scienceBlue: "#0366D6",
 iron: "D1D5DA",
 violet: "#090A3308",
+dodgerBlue: "#2188FF",
+semiDark: "rgba(54, 54, 54, 0.72)",
+semiGray: "rgba(209, 213, 218, 0.1)",
+
 };
 
 export const lightTheme = {
@@ -66,51 +70,51 @@ export const lightTheme = {
 export const darkTheme = {
     elementColor: {
         site: {
-            background: color.whiteLilac,
-            text: color.slateGray,
+            background: color.mineShaft,
+            text: color.white,
         },
         header: {
-            thisIs: color.slateGray,
-            name: color.mineShaft,
+            thisIs: color.white,
+            name: color.white,
         },
         buttonLink: {
             text: color.white,
             shadowHover: color.anakiwa,
-            background: color.scienceBlue
+            background: color.dodgerBlue
         },
         section: {
-            background: color.white,
-            header: color.mineShaft,
-            titleUnderLine: color.iron,
+            background: color.semiDark,
+            header: color.white,
+            titleUnderLine: color.semiGray,
         },
         list: {
-            text: color.slateGray,
-            eclipse: color.scienceBlue,
+            text: color.white,
+            eclipse: color.dodgerBlue,
         },
         footer: {
-            letsTalk: color.slateGray,
-            email: color.mineShaft,
-            icons: color.mineShaft,
-            aboutMe: color.mineShaft,
-            iconsHover: color.scienceBlue,
+            letsTalk: color.white,
+            email: color.white,
+            icons: color.white,
+            aboutMe: color.white,
+            iconsHover: color.dodgerBlue,
         },
         portfolio: {
-            subHeader: color.mineShaft,
-            title: color.mineShaft,
-            border: color.iron,
+            subHeader: color.white,
+            title: color.white,
+            border: color.semiGray,
             shadow: color.violet,
-            secondText: color.slateGray,
+            secondText: color.white,
             tileTitle: color.scienceBlue,
-            background: color.white,
-            tileLink: color.scienceBlue,
-            paragraph: color.mineShaft,
-            spinner: color.iron,
+            background: color.semiDark,
+            tileLink: color.dodgerBlue,
+            paragraph: color.white,
+            spinner: color.semiDark,
             spinnerColor: color.scienceBlue, 
         },
         themeSwitch: {
-            text: color.slateGray,
-            background: color.mercury,
-            switcherBackgroung: color.slateGray,
+            text: color.white,
+            background: color.semiDark,
+            switcherBackgroung: color.white,
         }
     
     }
